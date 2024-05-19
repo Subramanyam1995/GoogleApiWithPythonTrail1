bc033fee057ff8541d479e10a496dd16f0105da3
Subramanyam1995 <subramanyam04111995@gmail.com>
2024-05-19 10:47:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
-print(123)
+import gspread
+from google.oauth2.service_account import Credentials
+
+scopess = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+creads = Credentials.from_service_account_file("linkfile.json", scopes=scopess)
+client = gspread.authorize(creads)
+
+sheet_id = "1zXaPwVzrxH6YIAQRMKj2qDdpn1pxX8xVxglgUwuBqik"
+
+sheet = client.open_by_key(sheet_id)
+
+values_list = sheet.sheet1.row_values(1)
+
+print(values_list)
+
+
